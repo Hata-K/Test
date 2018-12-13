d55ba6fd16e469d28f304c8c6c2ddfb1da15d092
vidTHEkid <vidTHEkid@users.noreply.github.com>
2018-12-13 23:34:17 +0000

    more clean up

--- test2_assignment3/test2_assignment3/Program.cs
+++ test2_assignment3/test2_assignment3/Program.cs
@@ -15,41 +15,36 @@ namespace test2_assignment3
         {
             DirectoryInfo sourceDir = new DirectoryInfo("source.csv");
 
-            var y = File.ReadAllLines(sourceDir.ToString());
-            var x = File.ReadAllLines(sourceDir.ToString()).Length;
-            
-            List<String> No = new List<String>();
-            List<String> Name = new List<String>();
+            var fileLines = File.ReadAllLines(sourceDir.ToString());
+            var lineSum = File.ReadAllLines(sourceDir.ToString()).Length;
+
+            List<String> name = new List<String>();
+            List<String> nameEdited = new List<String>();
             List<String> dob = new List<String>();
             List<String> start = new List<String>();
             List<String> end = new List<String>();
 
-            for (int i = 1; i <= (x-1); i++)
+            for (int i = 1; i <= (lineSum - 1); i++)
             {
-                string[] eachLine = y[i].Split(',');
-                No.Add(eachLine[1]);
-                Name.Add(eachLine[1]);
+                string[] eachLine = fileLines[i].Split(',');
+                name.Add(eachLine[1]);
+                nameEdited.Add(eachLine[1]);
                 dob.Add(eachLine[2]);
                 start.Add(eachLine[3]);
                 end.Add(eachLine[4]);
             }
 
-            var dup = Name.GroupBy(i=>i).Where(g => g.Count() > 1).Select(g => g.Key);
-
-            foreach (var item in dup)
-            {
-                Console.WriteLine(item);
-            }
+            var nameDuplicates = nameEdited.GroupBy(i=>i).Where(g => g.Count() > 1).Select(g => g.Key);
 
             int count = 1;
-            foreach (var d in dup)
+            foreach (var d in nameDuplicates)
             {
-                for (int i = 0; i < Name.Count; i++)
+                for (int i = 0; i < nameEdited.Count; i++)
                 {
                 
-                    if (Name[i] == (d))
+                    if (nameEdited[i] == (d))
                     {
-                        Name[i] = (Name[i] +"-TERM-"+ count);
+                        nameEdited[i] = (nameEdited[i] +"-TERM-"+ count);
                         ++count;
                     }
                     else
@@ -59,19 +54,21 @@ namespace test2_assignment3
                 }
                 count = 1;
             }
-
+            //An int arry is initiated to store 3 random number from 1 to 77;
             int[] indexes = new int[3];
             
+            //Random funtion is initiated.
             Random rnd = new Random();
+            //Number is picked from 1 to 77
             int index = rnd.Next(1, 77);
-            
+            //0th place is placed with that random number
             indexes[0] = index;
             
             for (int i = 1; i < 3; i++)
             {
                 int ind = rnd.Next(1, 77);
                 
-                if (No[indexes[(i-1)]] != No[ind])
+                if (name[indexes[(i-1)]] != name[ind])
                 {
                     indexes[i] = ind;
                 }
@@ -81,50 +78,36 @@ namespace test2_assignment3
                 }
             }
 
+            //DISPLAYING THE SELCETED NAMES TO THE USER
             count = 1;
             foreach (var s in indexes)
             {
-                Console.WriteLine(count+": "+Name[s]);
+                Console.WriteLine(count+": "+nameEdited[s]);
                 count += 1;
             }
-
-            List<DateTime> dateCol = new List<DateTime>();
-
-            for (int i = 0; i < indexes.Length; i++)
-            {
-                DateTime oDate = Convert.ToDateTime(start[indexes[i]]);
-                dateCol.Add(oDate);
-            }
-
-            dateCol.Sort();
-            dateCol.Reverse();
-
-            for (int i = 0; i < dateCol.Count; i++)
-            {
-                string da = dateCol[i].ToString("dd/MM/yyyy");
-                Console.WriteLine(da);
-            }
-
-            Console.WriteLine("\n");
             var kList = new List<KeyValuePair<string, DateTime>>();
 
             for (int i = 0; i < indexes.Length; i++)
             {
                 DateTime oDate = Convert.ToDateTime(start[indexes[i]]);
-                kList.Add(new KeyValuePair<string, DateTime>(Name[indexes[i]],oDate));
+
+                kList.Add(new KeyValuePair<string, DateTime>(nameEdited[indexes[i]],oDate));
             }
             kList.Sort((a, b) => (b.Value.CompareTo(a.Value)));
             kList.Reverse();
+            Console.WriteLine("\n");
+
             foreach (var element in kList)
             {
-                Console.WriteLine(element);
+                Console.WriteLine(element.Key + "\t" + element.Value.ToShortDateString());
             }
+            Console.WriteLine("\n");
 
             Console.WriteLine("Who's the first president to run?...");
             int userInput = Convert.ToInt32(Console.ReadLine());
             int userInputIndex = userInput - 1;
                         
-            if (Name[indexes[userInputIndex]] == kList[0].Key)
+            if (nameEdited[indexes[userInputIndex]] == kList[0].Key)
             {
                 Console.WriteLine("Correct!");
                 Console.WriteLine(kList[0].Value.ToString("dd/MM/yyyy"));
@@ -135,8 +118,8 @@ namespace test2_assignment3
                 Console.WriteLine("The correct answer is: {0} --> {1}", kList[0].Key, kList[0].Value.ToString("dd/MM/yyyy"));
             }
 
+            //End of program.
             Console.ReadLine();
-
         }
     }
 }
