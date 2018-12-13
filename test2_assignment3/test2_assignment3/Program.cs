1854bb45fe5f31d81a5d4388f1efe99f72442140
vidTHEkid <vidTHEkid@users.noreply.github.com>
2018-12-13 23:01:21 +0000

    Clean Up
    Still works fine, just need to clean up with the naming next also comment!

--- test2_assignment3/test2_assignment3/Program.cs
+++ test2_assignment3/test2_assignment3/Program.cs
@@ -10,18 +10,13 @@ namespace test2_assignment3
 {
     internal class Program
     {
-        public class MultiDimDictList<K, T>: Dictionary<K, List<T>>  { }
-        
+                
         public static void Main(string[] args)
         {
             DirectoryInfo sourceDir = new DirectoryInfo("source.csv");
-            
-            //var myDicList = new MultiDimDictList<string, string> ();
-            var myDicList = new MultiDimDictList<string, List<String>>();;
 
             var y = File.ReadAllLines(sourceDir.ToString());
             var x = File.ReadAllLines(sourceDir.ToString()).Length;
-            //Console.WriteLine(x);
             
             List<String> No = new List<String>();
             List<String> Name = new List<String>();
@@ -32,37 +27,19 @@ namespace test2_assignment3
             for (int i = 1; i <= (x-1); i++)
             {
                 string[] eachLine = y[i].Split(',');
-                //Console.WriteLine(eachLine.Length);
-                //Console.WriteLine(eachLine[1] + " " + eachLine[2] + " " + eachLine[3] + " "+ eachLine[4]);
-                //myDicList.Add("No" , eachLine[0]);
                 No.Add(eachLine[1]);
                 Name.Add(eachLine[1]);
                 dob.Add(eachLine[2]);
                 start.Add(eachLine[3]);
                 end.Add(eachLine[4]);
-                for(int r = 1; r < eachLine.Length; r++)
-                {
-                    //Console.WriteLine(eachLine[r]);
-                    
-                }
             }
-            /*
-            foreach (var VARIABLE in Name)
-            {
-                
-                if (VARIABLE == ("Thomas Pelham-Holles"))
-                {
-                    
-                    VARIABLE = VARIABLE +"-TERM-"+ count);
-                    ++count;
-                }
-                else
-                {
-                    Console.WriteLine(VARIABLE);
-                }
-                
-            }*/
+
             var dup = Name.GroupBy(i=>i).Where(g => g.Count() > 1).Select(g => g.Key);
+
+            foreach (var item in dup)
+            {
+                Console.WriteLine(item);
+            }
 
             int count = 1;
             foreach (var d in dup)
@@ -72,33 +49,17 @@ namespace test2_assignment3
                 
                     if (Name[i] == (d))
                     {
-                    
                         Name[i] = (Name[i] +"-TERM-"+ count);
                         ++count;
-                        //Console.WriteLine(Name[i]);
                     }
                     else
                     {
-                        //Console.WriteLine(Name[i]);
                         continue;
                     }
                 }
                 count = 1;
             }
 
-            foreach (var VARIABLE in Name)
-            {
-                //Console.WriteLine(VARIABLE);
-            }
-            /*
-            int[] listOfItems = new[] { 4, 2, 3, 1, 6, 4, 3 };
-            var duplicates = listOfItems
-                .GroupBy(i => i)
-                .Where(g => g.Count() > 1)
-                .Select(g => g.Key);
-            foreach (var d in duplicates)
-                Console.WriteLine(d);  
-            */
             int[] indexes = new int[3];
             
             Random rnd = new Random();
@@ -123,12 +84,10 @@ namespace test2_assignment3
             count = 1;
             foreach (var s in indexes)
             {
-                //Console.WriteLine(s);
                 Console.WriteLine(count+": "+Name[s]);
                 count += 1;
             }
 
-            //DateTime oDate = Convert.ToDateTime(start[indexes[0]]);
             List<DateTime> dateCol = new List<DateTime>();
 
             for (int i = 0; i < indexes.Length; i++)
@@ -137,9 +96,9 @@ namespace test2_assignment3
                 dateCol.Add(oDate);
             }
 
-            Console.WriteLine(indexes.Length);
             dateCol.Sort();
             dateCol.Reverse();
+
             for (int i = 0; i < dateCol.Count; i++)
             {
                 string da = dateCol[i].ToString("dd/MM/yyyy");
@@ -164,20 +123,20 @@ namespace test2_assignment3
             Console.WriteLine("Who's the first president to run?...");
             int userInput = Convert.ToInt32(Console.ReadLine());
             int userInputIndex = userInput - 1;
-
-            int score = 0;
-            
+                        
             if (Name[indexes[userInputIndex]] == kList[0].Key)
             {
-                Console.WriteLine("xxxx");
-                score += 1;
+                Console.WriteLine("Correct!");
+                Console.WriteLine(kList[0].Value.ToString("dd/MM/yyyy"));
             }
             else
             {
-                Console.WriteLine("Please try again...");
+                Console.WriteLine("Oops! Wrong...");
+                Console.WriteLine("The correct answer is: {0} --> {1}", kList[0].Key, kList[0].Value.ToString("dd/MM/yyyy"));
             }
-            //Console.WriteLine(Name[indexes[userInputIndex]]);
-            Console.WriteLine(score);
+
+            Console.ReadLine();
+
         }
     }
 }
